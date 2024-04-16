70a68ca15f9a8b69ee1b869350a768072548eb30
Jun-nior <petertg1702@gmail.com>
2024-04-16 13:29:11 +0700

    fix setdevice

--- scheduler.py
+++ scheduler.py
@@ -21,6 +21,7 @@ def set_timeout(value):
 
 set_timeout(5)
 
+setDevice1(True,2)
 while True:
     # setDevice1(True,2)
     print("state:" + str(state) + " " + "time: " + str(time_out))
@@ -29,45 +30,43 @@ while True:
         if (time_out<=0) :
             print("MIXER 1: ")
             setDevice1(True,2)
-            time.sleep(2)
-            setDevice1(True,2)
             next_state=MIXER_1
             set_timeout(5)
-    # elif (state==MIXER_1):
-    #     time_out = time_out -1
-    #     if (time_out<=0) :
-    #         next_state=MIXER_2
-    #         set_timeout(5)
-    # elif (state==MIXER_2):
-    #     time_out = time_out -1
-    #     if (time_out<=0) :
-    #         next_state=MIXER_3
-    #         set_timeout(5)
-    # elif (state==MIXER_3):
-    #     time_out = time_out -1
-    #     if (time_out<=0) :
-    #         next_state=PUMP_IN
-    #         set_timeout(5)
-    # elif (state==PUMP_IN):
-    #     time_out = time_out -1
-    #     if (time_out<=0) :
-    #         next_state=SELECTOR
-    #         set_timeout(5)
-    # elif (state==SELECTOR):
-    #     time_out = time_out -1
-    #     if (time_out<=0) :
-    #         next_state=PUMP_OUT
-    #         set_timeout(5)
-    # elif (state==PUMP_OUT):
-    #     time_out = time_out -1
-    #     if (time_out<=0) :
-    #         next_state=NEXT_CYCLE
-    #         set_timeout(5)
-    # elif (state==NEXT_CYCLE):
-    #     time_out = time_out -1
-    #     if (time_out<=0) :
-    #         next_state=IDLE
-    #         set_timeout(5)
+    elif (state==MIXER_1):
+        time_out = time_out -1
+        if (time_out<=0) :
+            next_state=MIXER_2
+            set_timeout(5)
+    elif (state==MIXER_2):
+        time_out = time_out -1
+        if (time_out<=0) :
+            next_state=MIXER_3
+            set_timeout(5)
+    elif (state==MIXER_3):
+        time_out = time_out -1
+        if (time_out<=0) :
+            next_state=PUMP_IN
+            set_timeout(5)
+    elif (state==PUMP_IN):
+        time_out = time_out -1
+        if (time_out<=0) :
+            next_state=SELECTOR
+            set_timeout(5)
+    elif (state==SELECTOR):
+        time_out = time_out -1
+        if (time_out<=0) :
+            next_state=PUMP_OUT
+            set_timeout(5)
+    elif (state==PUMP_OUT):
+        time_out = time_out -1
+        if (time_out<=0) :
+            next_state=NEXT_CYCLE
+            set_timeout(5)
+    elif (state==NEXT_CYCLE):
+        time_out = time_out -1
+        if (time_out<=0) :
+            next_state=IDLE
+            set_timeout(5)
     state=next_state
     time.sleep(1)
     
